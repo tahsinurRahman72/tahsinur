fedf7dfd5c058ac79148a6a29ab39a5b5b59a394
Tahsinur Rahman <tahsinur72@gmail.com>
2024-08-23 05:44:43 +1000

    finished the education and publication section. Need to add menu bar, containerize dependency in docker and post data in MongoDB

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,18 +3,31 @@ import Image from "next/image";
 export default function Home() {
   return (
   <main className="flex min-h-screen flex-col justify-between p-12">
+    
     <div id="name" className="flex justify-start text-5xl font-sans text-white items-center">
       <h1>Tahsinur Rahman</h1>
     </div>
+      
+    {/* <div id="header" className="flex justify-between items-center p-4 bg-black">
+      <div id="name" className="text-5xl font-sans text-white">
+        <h1>Tahsinur Rahman</h1>
+      </div>
+      <div id="menu" className="flex space-x-6">
+        <button className="text-white font-sans text-lg">Experience</button>
+        <button className="text-white font-sans text-lg">Skills</button>
+        <button className="text-white font-sans text-lg">Education</button>
+        <button className="text-white font-sans text-lg">Publications</button>
+      </div>
+    </div> */}
 
     <div id="intro-and-photo"className="flex min-h-screen flex-col min-[1000px]:flex-row justify-around items-center"> 
       <div id="intro" className="flex">
-        <p className="p-12 pl-[0.7rem] mb-3 text-xl font-light">
+          <p className="p-12 pl-[0.7rem] mb-3 text-xl font-light"><i>
                             I'm Tahsinur Rahman, currently completing Master's in Data Science at La Trobe University,
-                            passionate about data manipulation, visualization, and automation with skills in MEVN and
-                            MERN stacks, Python, Java, and C++. Focused on advancing data engineering to enhance data
-                            quality and accessibility, I'm driven to unlock insights and innovate through data.
-        </p>
+                            passionate about data manipulation, visualization, and automation with language proficiency in Scala, Python,
+                            JavaScript and respective frameworks. Focused on advancing data engineering to enhance data quality and accessibility,
+                            I'm driven to unlock insights and innovate through data.
+          </i></p>
       </div>
       <Image
         className="relative rounded-e-full"
@@ -316,10 +329,10 @@ export default function Home() {
                 className="object-contain"
               />
             </div>
-            <div className="flex items-center justify-center min-w-[100px] max-w-[150px] max-h-[100px] invert">
+            <div className="flex items-center justify-center min-w-[100px] max-w-[150px] max-h-[100px]">
               <Image
-                src="/images/powerbi-svgrepo-com.svg"
-                alt="Power BI"
+                src="/images/docker-svgrepo-com.svg"
+                alt="Docker"
                 width={150}
                 height={100}
                 className="object-contain"
@@ -352,8 +365,8 @@ export default function Home() {
         <div className="flex flex-col lg:flex-row justify-between w-full">
           <div id="institution-info-left" className="flex flex-col">
             <h1>La Trobe University</h1>
-            <h2>Master of Data Science</h2>
-            <h2>Artificial Intelligence Analytics</h2>
+            <h2>Degree: Master of Data Science</h2>
+            <h2>Specialization: Artificial Intelligence Analytics</h2>
           </div>
           <div id="institution-info-right" className="flex flex-col lg:text-right mt-4 lg:mt-0">
             <h3>Melbourne, Victoria</h3>
@@ -375,7 +388,7 @@ export default function Home() {
         <div className="flex flex-col lg:flex-row justify-between w-full">
           <div id="institution-info-left" className="flex flex-col">
             <h1>BRAC University</h1>
-            <h2>Bachelor of Computer Science and Engineering</h2>
+            <h2>Degree: Bachelor of Computer Science and Engineering</h2>
             <h2>CGPA: 3.39/4.00</h2>
           </div>
           <div id="institution-info-right" className="flex flex-col lg:text-right mt-4 lg:mt-0">
@@ -386,13 +399,52 @@ export default function Home() {
       </div>   
     </div>
 
+    <br />
+    <br />
 
-    {/* <div id="publications-heading" className="flex justify-start text-5xl font-sans text-white items-center">
+    <div id="publications-heading" className="flex justify-start text-5xl font-sans text-white items-center">
       <h1>
         Publications
       </h1>
-    </div>     */}
+    </div>
+    
+    <div id="publication" className="flex flex-col">
+      <div className="flex-row mt-10 flex flex-wrap justify-center items-center gap-x-10 gap-y-10 sm:gap-x-14 lg:mx-0 lg:max-w-none lg:flex-nowrap rounded-md border-2 p-5">
+          <Image
+            className="relative"
+            src="/images/IEEE_logo.svg"
+            alt="IEEE"
+            width={200}
+            height={200}
+            priority
+          />
+          <h3 className="text-2xl font-semibold max-[320px]:text-base max-[375px]:text-lg max-[425px]:text-xl">
+            Blockchain and Machine Learning for Fraud Detection: A Privacy-Preserving and Adaptive Incentive Based Approach
+          </h3>
+          <h5 className="max-[320px]:text-xs max-[375px]:text-sm max-[425px]:text-base">
+            Financial fraud is increasing despite technological advancements, primarily due to a lack of inter-organizational collaboration and privacy concerns, which limit access to authentic financial data. This study proposes a blockchain and smart contract-based approach to enhance e-commerce fraud detection by enabling secure, collaborative data sharing, leading to a 98.93% testing accuracy and 98.22% F-beta score over eight incremental ML model updates.
+          </h5>
+      </div>
       
+      <div className="flex-row mt-10 flex flex-wrap justify-center items-center gap-x-10 gap-y-10 sm:gap-x-14 lg:mx-0 lg:max-w-none lg:flex-nowrap rounded-md border-2 p-5">
+          <Image
+            className="relative"
+            src="/images/IEEE_logo.svg"
+            alt="IEEE"
+            width={200}
+            height={200}
+            priority
+          />
+          <h3 className="text-2xl font-semibold max-[320px]:text-base max-[375px]:text-lg max-[425px]:text-xl">
+            Interpreting Machine and Deep Learning Models for PDF Malware Detection using XAI and SHAP Framework
+          </h3>
+          <h5 className="max-[320px]:text-xs max-[375px]:text-sm max-[425px]:text-base">
+            As data transfer in Portable Document Format (PDF) becomes widespread in the digital era, PDF files are increasingly vulnerable to malware attacks that traditional anti-virus software may not effectively detect. This study proposes using Explainable AI (XAI) with the SHAP framework to enhance transparency in classifying PDF files as malicious or clean, employing models like SGD, XGBoost, Single Layer Perceptron, and ANN.
+          </h5>
+      </div>  
+    </div>
+      
+
   </main>
 
   );
