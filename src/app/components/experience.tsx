47582946d6607994758dab74b2889d7377f0565f
Tahsinur Rahman <tahsinur72@gmail.com>
2024-08-24 16:10:22 +1000

    added Dockerfile, segregated sections and optimized resolution based sizes

---
+++ src/app/components/experience.tsx
@@ -0,0 +1,93 @@
+import Image from 'next/image';
+
+export default function Experience() {
+    return (
+    <>
+    <div id="experience-heading" className="flex justify-start text-5xl min-[2560px]:text-8xl font-sans text-white items-center min-[2560px]:m-12">
+        <h1>
+        Experience
+        </h1>
+    </div>
+
+    <br />
+        
+    <div id="experiences" className="flex min-[1000px]:space-x-3 flex-col min-[1000px]:flex-row max-[1000px]:items-center max-[1000px]:space-y-2 justify-center min-[2560px]:m-12">
+    <div className="expbox 
+        transition-colors
+        hover:dark:bg-teal-700
+        min-[2560px]:p-10">
+        <Image
+        className="relative"
+        src="/images/invtech.svg"
+        alt="invisible-technologies"
+        width={200}
+        height={200}
+        priority
+        />
+        <br />
+        <h3 className="mb-3 text-xl min-[2560px]:text-3xl font-semibold">
+            Invisible Technologies | Advanced AI Data Trainer
+        </h3>
+        <h6 className="min-[2560px]:text-xl">San Francisco, California | Jul 2022 - Aug 2024</h6>
+        <br />
+        <ul className="m-0 max-w-[50ch] min-[2560px]:text-2xl text-balance text-sm">
+            <li>Responsible for taking a deep dive on data driven behavior of machine models and reporting anomalies in such results.</li>
+            <br />  
+            <li>Enhancing model&apos;s approach on data by implementing various methods to improve behavior that benefits on the user end.</li>
+        </ul>
+    </div>
+        
+    <div className="expbox
+        transition-colors
+        hover:dark:bg-teal-700
+        min-[2560px]:p-10">
+        <Image
+        className="relative"
+        src="/images/daraz.png"
+        alt="daraz"
+        width={100}
+        height={100}
+        priority
+        />
+        <br />
+        <h3 className="mb-3 text-xl min-[2560px]:text-3xl font-semibold">
+            Daraz (Hungrynaki) | Product (Tech) Intern
+        </h3>
+        <h6 className="min-[2560px]:text-xl">Dhaka, Bangladesh | Feb 2022 - May 2022</h6>
+        <br />
+        <ul className="m-0 max-w-[50ch] min-[2560px]:text-2xl text-balance text-sm">
+            <li>Handled API Documentation on GraphQL schemas of the entire product codebase to ensure better understanding of each API</li>
+            <br />  
+            <li>Conducted breakdown explanation of logical calculations of each API present in the codebase</li>
+        </ul>
+    </div>
+
+    <div className="expbox 
+        transition-colors
+        hover:dark:bg-teal-700
+        min-[2560px]:p-10">
+        <Image
+        className="relative"
+        src="/images/intelligent-tr.png"
+        alt="intelligent-machines"
+        width={150}
+        height={100}
+        priority
+        />
+        <br />
+        <h3 className="mb-3 text-xl min-[2560px]:text-3xl font-semibold">
+            Intelligent Machines | AI Data Annotator
+        </h3>
+        <h6 className="min-[2560px]:text-xl">Dhaka, Bangladesh | Mar 2020 - Nov 2021</h6>
+        <br />
+        <ul className="m-0 max-w-[50ch] min-[2560px]:text-2xl text-balance text-sm">
+            <li>Annotated specific keywords recorded in audio clips and labeled them with corresponding tags based on requirements using custom annotation hosting servers provided by the clients</li>
+            <br />  
+            <li>Annotated specific words on images and labeled them with corresponding tags based on requirements using custom annotation hosting servers provided by the clients</li>
+        </ul>
+    </div>  
+    </div>
+    </>  
+    
+  );
+}
---
+++ src/app/components/header.tsx
@@ -0,0 +1,49 @@
+import { useState } from 'react';
+
+interface HeaderProps {
+  handleScroll: (sectionId: string) => void;
+}
+
+export default function Header({ handleScroll }: HeaderProps) {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
+  return (
+    <div>
+      <div id="header" className="flex justify-between items-center bg-black min-[2560px]:m-8">
+        <div id="name" className="text-5xl max-[320px]:text-4xl min-[2560px]:text-8xl font-sans text-white">
+          <h1>Tahsinur Rahman</h1>
+        </div>
+
+        <div id="menu" className="hidden md:flex max-[768px]:flex-col max-[768px]:items-start max-[768px]:space-x-reverse space-x-6">
+          <button onClick={() => handleScroll('experience-heading')} className="text-white font-sans text-2xl min-[2560px]:text-5xl">Experience</button>
+          <button onClick={() => handleScroll('skills-heading')} className="text-white font-sans text-2xl min-[2560px]:text-5xl">Skills</button>
+          <button onClick={() => handleScroll('education-heading')} className="text-white font-sans text-2xl min-[2560px]:text-5xl">Education</button>
+          <button onClick={() => handleScroll('publications-heading')} className="text-white font-sans text-2xl min-[2560px]:text-5xl">Publications</button>
+          <button onClick={() => handleScroll('contact-heading')} className="text-white font-sans text-2xl min-[2560px]:text-5xl">Contact</button>
+        </div>
+
+        <div className="md:hidden">
+          <button onClick={toggleMenu} className="text-white text-lg">
+            <svg className="w-8 h-8" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16"></path>
+            </svg>
+          </button>
+        </div>
+      </div>
+
+      {isOpen && (
+        <div className="bg-black text-white p-4 md:hidden">
+          <button onClick={() => handleScroll('experience-heading')} className="block font-sans text-xl w-full text-left py-2">Experience</button>
+          <button onClick={() => handleScroll('skills-heading')} className="block font-sans text-xxl w-full text-left py-2">Skills</button>
+          <button onClick={() => handleScroll('education-heading')} className="block font-sans text-xxl w-full text-left py-2">Education</button>
+          <button onClick={() => handleScroll('publications-heading')} className="block font-sans text-xxl w-full text-left py-2">Publications</button>
+          <button onClick={() => handleScroll('contact-heading')} className="block font-sans text-xxl w-full text-left py-2">Contact</button>
+        </div>
+      )}
+    </div>
+  );
+}
